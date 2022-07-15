cadb50cb2ee9aa4811c1ffb032d74272d51a7af5
Mark <markezon2@gmail.com>
2022-07-16 00:33:42 +0200

    Add fucking slider in section "review "

--- dist/js/script.js
+++ dist/js/script.js
@@ -74,3 +74,22 @@ validation
   });
 
 
+  
+  const slider = document.querySelector('.swiper-container');
+
+  let swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 50,
+    centeredSlides: true,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
