ba5701e354772e3ee5f0dbcc6d1bf5791e52b3f6
Mark <markezon2@gmail.com>
2022-07-19 16:44:16 +0200

    add Menu, links from menu, add pageUP, add scss for error input message

--- dist/js/script.js
+++ dist/js/script.js
@@ -92,6 +92,20 @@ validation
     },
   });
 
+  const hamburger = document.querySelector('.hamburger'),
+	menu = document.querySelector('.menu'),
+	closeElem = document.querySelector('.menu__close');
+
+  hamburger.addEventListener('click', () => {
+    menu.classList.add('active'),
+    hamburger.classList.add('rotate');
+  });
+
+  closeElem.addEventListener('click', () => {
+    menu.classList.remove('active'),
+    hamburger.classList.remove('rotate');
+  });
+
 
 
   
@@ -111,6 +125,28 @@ $(document).ready(function(){
 
   toggleSlide('.rates__item-btn');
   toggleSlide('.rates__item-btnback');
+
+
+
+  $(window).scroll(function() {
+		if ($(this).scrollTop() > 1000) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+	
+	$("a[href^='#']").click(function(){
+		const _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
+
+	$("a[href^='#']").click(function(){
+		const _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
 
 });
   
